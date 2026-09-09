404d857f85d5500640fc59929c19fe73a1b391f8
Ahmad Mujtaba <hanankhawar123@gmail.com>
2026-09-09 18:55:55 +0500

    update

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -9,8 +9,6 @@ export default function StyledComponentsRegistry({
 }: {
   children: React.ReactNode;
 }) {
-  // only create stylesheet once with lazy initial state
-  // x ref httpsreactjsorgdocshooks referencehtmllazy initial state
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
   useServerInsertedHTML(() => {
